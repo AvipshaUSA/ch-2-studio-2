8da2609673ac0566864611410ba3935f5873eb62
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-09-29 16:15:25 -0500

    Add project files.

---
+++ studio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            float r;
+            Console.WriteLine("enter radious");
+               input= Console.ReadLine();
+             r = float.Parse(input);
+            int i = 1;
+            while (i > 0)
+            {
+                if (r > 0)
+                {
+                    break;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("enter Posetive number");
+                    input = Console.ReadLine();
+                    r = float.Parse(input);
+                    i++;
+                }
+            }
+            Console.WriteLine("Press A to get Area Or Press C to get circumference:");
+            string s = Console.ReadLine();
+            circle.CircleA(r, s);
+            
+        }
+    }
+}
---
+++ studio/circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace studio
+{
+    class circle
+    {
+        public static void CircleA(float r, string str)
+        {
+            int i = 0;
+            
+            if (str.Equals("A"))
+            {
+                Console.WriteLine($"The Area of Circle is {Math.Round((Math.PI * r * r), 3)} with a radious of {r}");
+            }else if (str.Equals("C")){
+                Console.WriteLine($"The Circumference of Circle is {Math.Round((Math.PI * r * 2), 3)} with a radious of {r}");
+
+                }
+              
+
+        }
+    }
+}
